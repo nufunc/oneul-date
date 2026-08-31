f2c5df09722fb4668be189428c0a738baefb6c5b
nufunc <nufunc@gmail.com>
2026-08-31 21:37:21 +0900

    chore(release): bump version to 0.9.18

--- src/main.ts
+++ src/main.ts
@@ -2095,7 +2095,7 @@ function activeSlots(): SlotKey[] {
 }
 
 declare const __APP_VERSION__: string;
-const APP_VERSION = typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : 'v0.9.17';
+const APP_VERSION = typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : 'v0.9.18';
 
 function courseSpotIds(): number[] {
   if (!state.course) return [];
